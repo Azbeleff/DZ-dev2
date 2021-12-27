263717c299375cc50681f903296ec05da8cbd342
Nick <you@example.com>
2021-12-27 21:54:20 +0300

    sgsgs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// **Задача**: 
+// Написать программу, которая из имеющегося массива целых чисел формирует массив из чисел, больших 8. 
+// Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void FillArray(int[] newarray)
+{
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        newarray[i] = new Random().Next(1, 20);
+    }
+}
+
+void PrintArray(int[] newarray)
+{
+    for (int i = 0; i < newarray.Length; i++)
+    {
+        Console.Write($"{newarray[i]} ");
+    }
+}
+
+int newarraysize(int[] array)
+{
+    int targetarraylength = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 8)
+        {
+            targetarraylength++;
+        }
+    }
+    return targetarraylength;
+}
+
+void Fillg8array(int[] newarray, int [] targetarray)
+{
+    for (int j = 0, k = 0; j < newarray.Length; j++)
+    {
+        if (newarray[j] > 8)
+        {
+            targetarray[k] = newarray[j];
+            k++;
+        }
+    }
+}
+
+void Demo ()
+{
+    int arraylength = 10;
+    int[] sourcearray = new int[arraylength];
+    FillArray(sourcearray);
+    PrintArray(sourcearray);
+    Console.WriteLine("");
+    int[] targetarray = new int [newarraysize(sourcearray)];
+    Fillg8array (sourcearray, targetarray);
+
+    PrintArray(targetarray);
+}
+
+Demo ();
